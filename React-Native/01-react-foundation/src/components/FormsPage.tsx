f56757ad3164639143541803196ad467713876ec
Mario Cezzare <mcezzare@gmail.com>
2024-10-19 02:07:31 -0300

    some things on forms
    Source from section https://github.com/Klerith/RN-reforzamiento-react

--- React-Native/01-react-foundation/src/components/FormsPage.tsx
+++ React-Native/01-react-foundation/src/components/FormsPage.tsx
@@ -9,26 +9,33 @@ type FormInputs = {
 
 const FormsPage = () => {
   
-  const { register } = useForm<FormInputs>({
+  const { register , handleSubmit, formState, watch } = useForm<FormInputs>({
     defaultValues: {
       email:'mcezzare@gmail.com',
       password: 'Abc123'
     }
   });
   
+  const onSubmit = (myForm: FormInputs) => {
+      console.log(myForm)
+  }
   
+  console.log(watch('email'));
+
   return (
     <>
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
     <h3>FormsPage</h3>
 
     <div style={{display:'flex' , flexDirection:'column'}}>
-      <input type="text" placeholder="Email" {...register('email')} />
+      <input type="text" placeholder="Email" {...register('email',{required:true})} />
       <input type="text" placeholder="password" {...register('password')}/>
       <button type="submit">Entrar</button>
     </div>
     </form>
-    
+    <pre>
+      {JSON.stringify(formState,null,2)}
+    </pre>
     </>
   )
 }
