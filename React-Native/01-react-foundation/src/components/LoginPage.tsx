d355ca499c6c00f99e39f0994e1439340b3cd8a5
Mario Cezzare <mcezzare@gmail.com>
2024-10-18 22:01:45 -0300

    example of login/logout state with zustand

--- React-Native/01-react-foundation/src/components/LoginPage.tsx
+++ React-Native/01-react-foundation/src/components/LoginPage.tsx
@@ -1,18 +1,50 @@
-import { useAuthStore } from "../store/auth.store"
+import { useEffect } from "react";
+import { AuthUserState, useAuthStore } from "../store/auth.store";
 
 const LoginPage = () => {
- 
-    const authStatus = useAuthStore( state => state.status);
- 
-    return (
 
-        <>
-        <h3>Login Page</h3>
+  const authStatus = useAuthStore( state => state.status );
+  const user = useAuthStore( state => state.user );
 
-        {authStatus}
-        </>
-  
-  )
-}
+  const login = useAuthStore( state => state.login );
+  const logout = useAuthStore( state => state.logout );
 
-export default LoginPage
+  useEffect( () => {
+    setTimeout( () => {
+      logout();
+    }, 1500 );
+  }, [] );
+
+
+  if ( authStatus === AuthUserState.checking ) {
+    return <h3>Loading....</h3>;
+  }
+
+
+  return (
+
+    <>
+      <h3>Login Page</h3>
+      {/*  Check if authenticated block */ }
+      {
+        ( authStatus === AuthUserState.authenticated )
+          ? <div> Autenticado como { JSON.stringify( user, null, 2 ) }</div>
+          : <div> Não Autenticado </div>
+      }
+      {/*  Login/Logout  block */ }
+      {
+        ( authStatus === AuthUserState.authenticated )
+          ? (
+            <button onClick={ logout }>Logout</button>
+          )
+          : (
+            <button onClick={ () => login( 'mcezzare@gmail.com', 'ABC123' ) }>Login</button>
+          )
+      }
+
+    </>
+
+  );
+};
+
+export default LoginPage;
--- React-Native/01-react-foundation/src/store/auth.store.ts
+++ React-Native/01-react-foundation/src/store/auth.store.ts
@@ -1,16 +1,42 @@
 import { create } from "zustand";
 
 interface AuthState {
-    status: 'authenticated' | 'unauthenticated' | 'checking';
+    status: AuthUserState.authenticated | AuthUserState.unauthenticated | AuthUserState.checking
     token?: string;
     user?: {
         name: string;
         email: string;
-    }
+    },
+    login: (email: string, password: string) => void;
+    logout: () => void
 }
 
+export enum AuthUserState {
+    authenticated = 'authenticated',
+    unauthenticated = 'unauthenticated',
+    checking=  'checking',
+}
+
+
 export const useAuthStore = create<AuthState>()((set) => ({
-    status: 'checking',
+    status: AuthUserState.checking,
     token: undefined,
-    user: undefined
+    user: undefined,
+    login: (email: string, password:string) => {
+        set({
+            status: AuthUserState.authenticated,
+            token: 'ABC123',
+            user: {
+                name:'Mario',
+                email: email
+            }
+        });
+    },
+    logout: () => {
+        set({
+            status :AuthUserState.unauthenticated,
+            token : undefined,
+            user: undefined,
+        })
+    },
 }))
